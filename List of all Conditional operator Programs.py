42b872f63e7d4e691353d1f749662105b22d4e3f
Kalyan4454 <srinivasakalyan21@gmail.com>
2024-12-18 20:26:07 +0530

    Create List of all Conditional operator Programs.py

---
+++ List of all Conditional operator Programs.py
@@ -0,0 +1,40 @@
+'''
+16 Write a C program to find maximum between two numbers using conditional/ternary operator.
+17 Write a C program to find maximum between three numbers using conditional/ternary operator.
+18 Write a C program to check whether a number is even or odd using conditional/ternary operator.
+19 Write a C program to check whether year is leap year or not using conditional/ternary operator.
+20 Write a C program to check whether character is an alphabet or not using conditional/ternary operator.
+'''
+
+'''a,b=int(input()),int(input())
+if(a>b):
+    print(a,"is big")
+else:
+    print(b,"is big")'''
+
+'''a,b,c=int(input()),int(input()),int(input())
+if(a>b and a>c):
+    print(a,"is big")
+elif (b>a and b>c):
+    print(b,"is big")
+else:
+    print(c,"is big")'''
+
+'''a=int(input())
+if a%2==0:
+    print("Even")
+else:
+    print("Odd")'''
+
+'''a=int(input())
+if a%4==0 and a%100!=0 or a%400==0 :
+    print("leap year")
+else:
+    print("not leapyear")'''
+
+'''ch=input()
+print(ch.isalpha())
+if((ord(ch)>=65 and ord(ch)<=90) or (ord(ch)>=97 and ord(ch)<=122)):
+    print("Alphabet")
+else:
+    print("Not")'''
