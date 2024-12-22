fc6d7babda027ff5acedfc710af36c5e8c917a66
Kalyan4454 <srinivasakalyan21@gmail.com>
2024-12-22 21:17:05 +0530

    Update Basicprograms.py

--- Basicprograms.py
+++ Basicprograms.py
@@ -1,19 +1,19 @@
 '''
-1 Write a C program to enter two numbers and find their sum.
-2 Write a C program to enter two numbers and perform all arithmetic operations.
-3 Write a C program to enter length and breadth of a rectangle and find its perimeter.
-4 Write a C program to enter length and breadth of a rectangle and find its area.
-5 Write a C program to enter radius of a circle and find its diameter, circumference and area.
-6 Write a C program to enter length in centimeter and convert it into meter and kilometer.
-7 Write a C program to enter temperature in °Celsius and convert it into °Fahrenheit.
-8 Write a C program to convert days into years, weeks and days.
-9 Write a C program to find power of any number xy (x^y).
-10 Write a C program to enter any number and calculate its square root.
-11 Write a C program to enter two angles of a triangle and find the third angle.
-12 Write a C program to enter base and height of a triangle and find its area.
-13 Write a C program to calculate area of an equilateral triangle.
-14 Write a C program to enter marks of five subjects and calculate total, average and percentage.
-15 Write a C program to enter P, T, R and calculate Simple Interest.
+1 Write a  program to enter two numbers and find their sum.
+2 Write a  program to enter two numbers and perform all arithmetic operations.
+3 Write a  program to enter length and breadth of a rectangle and find its perimeter.
+4 Write a  program to enter length and breadth of a rectangle and find its area.
+5 Write a  program to enter radius of a circle and find its diameter, circumference and area.
+6 Write a  program to enter length in centimeter and convert it into meter and kilometer.
+7 Write a  program to enter temperature in °Celsius and convert it into °Fahrenheit.
+8 Write a  program to convert days into years, weeks and days.
+9 Write a  program to find power of any number xy (x^y).
+10 Write a  program to enter any number and calculate its square root.
+11 Write a  program to enter two angles of a triangle and find the third angle.
+12 Write a  program to enter base and height of a triangle and find its area.
+13 Write a  program to calculate area of an equilateral triangle.
+14 Write a  program to enter marks of five subjects and calculate total, average and percentage.
+15 Write a  program to enter P, T, R and calculate Simple Interest.
 '''
 
 '''a=input("enter 2 numbers :")
